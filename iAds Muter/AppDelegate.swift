70f001f8cba06a3b7719e7cf7717681c70d74cf3
Hoang Tran <hoangt@insideradius.com>
2015-08-20 15:12:31 -0700

    Initial Commit

---
+++ iAds Muter/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  iAds Muter
+//
+//  Created by Hoang Tran on 8/20/15.
+//  Copyright (c) 2015 Hoang Tran. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
