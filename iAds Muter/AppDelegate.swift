003ca9adb14bf0b19d2073f49e7d35f3e90754ed
Hoang Tran <hoangt@insideradius.com>
2015-08-20 16:20:10 -0700

    Running app.

--- iAds Muter/AppDelegate.swift
+++ iAds Muter/AppDelegate.swift
@@ -10,18 +10,77 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     @IBOutlet weak var window: NSWindow!
-
-
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2.0)
+    let enableMenuItem = NSMenuItem(title: "Enable", action: Selector("registerForItunesNotifications"), keyEquivalent: "e")
+    let disableMenuItem = NSMenuItem(title: "Disable", action: Selector("unregisterForItunesNotifications"), keyEquivalent: "d")
+    let menu = NSMenu()
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            
+            menu.addItem(NSMenuItem.separatorItem())
+            menu.addItem(NSMenuItem(title: "Quit", action: Selector("terminate:"), keyEquivalent: "q"))
+            statusItem.menu = menu
+            registerForItunesNotifications();
+        }
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+    
+    func applicationWillTerminate (aNotification: NSNotification) {
     }
-
-
+    
+    func registerForItunesNotifications () {
+        if let enableItem = menu.itemWithTitle("Enable") {
+            menu.removeItem(enableMenuItem)
+        }
+        
+        menu.insertItem(disableMenuItem, atIndex: 0)
+        NSDistributedNotificationCenter.defaultCenter().addObserver(self, selector:Selector("receivedNotification:") , name: "com.apple.iTunes.playerInfo", object: nil)
+    }
+    
+    func unregisterForItunesNotifications () {
+        if let disableItem = menu.itemWithTitle("Disable") {
+            menu.removeItem(disableMenuItem)
+        }
+        menu.insertItem(enableMenuItem, atIndex: 0)
+        NSDistributedNotificationCenter.defaultCenter().removeObserver(self, name: "com.apple.iTunes.playerInfo", object: nil)
+    }
+    
+    func receivedNotification(notification: NSNotification){
+        if let userInfo = notification.userInfo {
+            println(userInfo)
+            if(userInfo["nomenR"]?.length > 0) {
+                unmute()
+            }
+            else {
+                mute()
+            }
+        }
+        
+    }
+    
+    func mute () {
+        if let script = NSAppleScript(source: "set volume with output muted") {
+            var errorDict : NSDictionary?
+            script.executeAndReturnError(&errorDict)
+            if let hasError = errorDict {
+                println("Error muting: \(hasError)")
+            }
+        }
+    }
+    
+    func unmute () {
+        if let script = NSAppleScript(source: "set volume without output muted") {
+            var errorDict : NSDictionary?
+            script.executeAndReturnError(&errorDict)
+            if let hasError = errorDict {
+                println("Error unmuting: \(hasError)")
+            }
+        }
+        
+    }
+    
 }
 
